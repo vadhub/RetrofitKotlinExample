91360947be2c69b56867ee81ea7099cb3df77371
Vadim <gabderahmanov99@gmail.com>
2021-11-13 23:25:12 +0300

    get users and to add in recycler

--- app/src/main/java/com/vad/retrofitkotlinexample/MainActivity.kt
+++ app/src/main/java/com/vad/retrofitkotlinexample/MainActivity.kt
@@ -3,7 +3,10 @@ package com.vad.retrofitkotlinexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vad.retrofitkotlinexample.api.RetrofitService
 import com.vad.retrofitkotlinexample.common.Common
 import com.vad.retrofitkotlinexample.entity.User
@@ -15,29 +18,24 @@ import retrofit2.Response
 class MainActivity() : AppCompatActivity() {
 
     lateinit var retrofitService: RetrofitService
-    lateinit var textViewUsers: TextView
+    lateinit var myRecycler: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textViewUsers = findViewById(R.id.textRequest)
+        myRecycler = findViewById(R.id.myRecycler)
         retrofitService = Common.retrofitService
-    }
 
-    fun onGetRequest(view: View) {
+        myRecycler.layoutManager = LinearLayoutManager(this)
+
         getUsers()
     }
 
     fun getUsers() {
         retrofitService.getUsers().enqueue(object : Callback<MutableList<User>> {
             override fun onResponse(call: Call<MutableList<User>>, response: Response<MutableList<User>>) {
-                var result = ""
-                response.body()?.toList()?.forEach {
-                    result += "${it.id} ${it.title} ${it.body} \n"
-                }
-
-                textViewUsers.text = result
+                myRecycler.adapter = response.body()?.let { UsersAdapter(it.toList()) }
             }
 
             override fun onFailure(call: Call<MutableList<User>>, t: Throwable) {
