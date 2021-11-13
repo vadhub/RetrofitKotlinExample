9238135623428b29a4289662b9b7fd47996aa15c
Vadim <gabderahmanov99@gmail.com>
2021-11-13 23:14:17 +0300

    add adapter on list user & user item

---
+++ app/src/main/java/com/vad/retrofitkotlinexample/UsersAdapter.kt
@@ -0,0 +1,40 @@
+package com.vad.retrofitkotlinexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vad.retrofitkotlinexample.entity.User
+
+class UsersAdapter(private val users: List<User>) : RecyclerView.Adapter<UsersAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.user_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.id?.text = users[position].id.toString()
+        holder.title?.text = users[position].title
+        holder.body?.text = users[position].body
+
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var id: TextView? = null
+        var title: TextView? = null
+        var body: TextView? = null
+
+        init {
+            id = itemView.findViewById(R.id.textViewId)
+            title = itemView.findViewById(R.id.textViewTitle)
+            body = itemView.findViewById(R.id.textViewBody)
+        }
+    }
+}
