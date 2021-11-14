6e508f72a0ea4dfb3e965c34a2b41363ab2b2171
Vadim <gabderahmanov99@gmail.com>
2021-11-14 14:59:32 +0300

    move logic into ListFragment.kt

--- app/src/main/java/com/vad/retrofitkotlinexample/screens/list/ListFragment.kt
+++ app/src/main/java/com/vad/retrofitkotlinexample/screens/list/ListFragment.kt
@@ -5,14 +5,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vad.retrofitkotlinexample.R
+import com.vad.retrofitkotlinexample.api.RetrofitService
+import com.vad.retrofitkotlinexample.common.Common
+import com.vad.retrofitkotlinexample.entity.User
+import com.vad.retrofitkotlinexample.screens.adapter.UsersAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListFragment : Fragment() {
+
+    lateinit var myRecycler: RecyclerView
+    lateinit var retrofitService: RetrofitService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retrofitService = Common.retrofitService
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.list_fragment, container, false)
+    ): View {
+        val view: View = inflater.inflate(R.layout.list_fragment, container, false)
+        myRecycler = view.findViewById(R.id.myRecycler)
+        myRecycler.layoutManager = LinearLayoutManager(view.context)
+        getUsers()
+        return view
     }
+
+    fun getUsers() {
+        retrofitService.getUsers().enqueue(object : Callback<MutableList<User>>{
+            override fun onResponse(
+                call: Call<MutableList<User>>,
+                response: Response<MutableList<User>>
+            ) {
+                myRecycler.adapter = response.body()?.toList()?.let { UsersAdapter(it) }
+            }
+
+            override fun onFailure(call: Call<MutableList<User>>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+    }
+
+
 }
--- app/src/main/java/com/vad/retrofitkotlinexample/screens/main/MainActivity.kt
+++ app/src/main/java/com/vad/retrofitkotlinexample/screens/main/MainActivity.kt
@@ -2,45 +2,14 @@ package com.vad.retrofitkotlinexample.screens.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.vad.retrofitkotlinexample.R
-import com.vad.retrofitkotlinexample.api.RetrofitService
-import com.vad.retrofitkotlinexample.common.Common
-import com.vad.retrofitkotlinexample.entity.User
-import com.vad.retrofitkotlinexample.screens.adapter.UsersAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
+import com.vad.retrofitkotlinexample.screens.list.ListFragment
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var retrofitService: RetrofitService
-    lateinit var myRecycler: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        myRecycler = findViewById(R.id.myRecycler)
-        retrofitService = Common.retrofitService
-
-        myRecycler.layoutManager = LinearLayoutManager(this)
-
-        getUsers()
-    }
-
-    private fun getUsers() {
-        retrofitService.getUsers().enqueue(object : Callback<MutableList<User>> {
-            override fun onResponse(call: Call<MutableList<User>>, response: Response<MutableList<User>>) {
-                myRecycler.adapter = response.body()?.let { UsersAdapter(it.toList()) }
-            }
-
-            override fun onFailure(call: Call<MutableList<User>>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, ListFragment()).commit()
     }
 }
