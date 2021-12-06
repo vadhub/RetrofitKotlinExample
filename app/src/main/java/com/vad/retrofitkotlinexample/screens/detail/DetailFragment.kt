eeb4cd25538ca0ecb56d2c7229d8510db2a259f0
Vadim <gabderahmanov99@gmail.com>
2021-12-06 21:43:46 +0300

    get arguments string from fragment

--- app/src/main/java/com/vad/retrofitkotlinexample/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/vad/retrofitkotlinexample/screens/detail/DetailFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.vad.retrofitkotlinexample.R
 
 class DetailFragment : Fragment() {
 
     private var userId: String =""
-    private var title: String = ""
-    private var body: String = ""
+
+    private lateinit var userIdTextView: TextView
+    private lateinit var titleTextView: TextView
+    private lateinit var bodyTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,7 +22,15 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view: View = inflater.inflate(R.layout.detail_fragment, container, false)
+        userIdTextView = view.findViewById(R.id.textViewIdUser)
+        titleTextView = view.findViewById(R.id.textViewTitle)
+        bodyTextView = view.findViewById(R.id.textViewBody)
 
+        arguments?.getString("id")?.let { userId = it }
+
+        userIdTextView.text = userId
+        titleTextView.text = arguments?.getString("title").toString()
+        bodyTextView.text = arguments?.getString("body").toString()
         return view
     }
 }
