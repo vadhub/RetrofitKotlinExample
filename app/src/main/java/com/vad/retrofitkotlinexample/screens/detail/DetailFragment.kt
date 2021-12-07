41842b07ff95c1f1c9aaaeedeede55ae5896e17b
Vadim <gabderahmanov99@gmail.com>
2021-12-07 20:30:09 +0300

    add set title

--- app/src/main/java/com/vad/retrofitkotlinexample/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/vad/retrofitkotlinexample/screens/detail/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.OnBackPressedDispatcher
 import androidx.fragment.app.Fragment
 import com.vad.retrofitkotlinexample.R
 
@@ -14,18 +15,25 @@ class DetailFragment : Fragment() {
     private lateinit var titleTextView: TextView
     private lateinit var bodyTextView: TextView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        activity?.actionBar?.title = arguments?.getInt("id").toString()
+
         var view: View = inflater.inflate(R.layout.detail_fragment, container, false)
 
         userIdTextView = view.findViewById(R.id.textViewIdUser)
         titleTextView = view.findViewById(R.id.textViewTitleDetail)
         bodyTextView = view.findViewById(R.id.textViewBody)
 
-        userIdTextView.text = arguments?.getInt("id").toString()
+        //userIdTextView.text = arguments?.getInt("id").toString()
         titleTextView.text = arguments?.getString("title")
         bodyTextView.text = arguments?.getString("body")
         return view
