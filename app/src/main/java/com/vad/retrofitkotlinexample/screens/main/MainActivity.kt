a4b4dbf279b73e3a59f8786d27027fb68b334bf9
Vadim <gabderahmanov99@gmail.com>
2021-12-13 22:41:25 +0300

    realisation navigation fragment

--- app/src/main/java/com/vad/retrofitkotlinexample/screens/main/MainActivity.kt
+++ app/src/main/java/com/vad/retrofitkotlinexample/screens/main/MainActivity.kt
@@ -2,16 +2,54 @@ package com.vad.retrofitkotlinexample.screens.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.vad.retrofitkotlinexample.R
+import com.vad.retrofitkotlinexample.screens.contract.HasTitle
 import com.vad.retrofitkotlinexample.screens.list.ListFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val currentFragment: Fragment? get() = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+    private val fragmentListener = object: FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentViewCreated(
+            fm: FragmentManager,
+            f: Fragment,
+            v: View,
+            savedInstanceState: Bundle?
+        ) {
+            super.onFragmentViewCreated(fm, f, v, savedInstanceState)
+            updateOnUI()
+        }
+    }
+
+    private fun updateOnUI() {
+        val fragment = currentFragment
+        title = if (fragment is HasTitle) {
+            fragment.getTitle()
+        } else {
+            getString(R.string.app_name)
+        }
+
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            actionBar?.setDisplayHomeAsUpEnabled(true)
+            actionBar?.setDisplayHomeAsUpEnabled(true)
+        } else {
+            actionBar?.setDisplayHomeAsUpEnabled(false)
+            actionBar?.setDisplayHomeAsUpEnabled(false)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        actionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentListener, false)
+
         supportFragmentManager
             .beginTransaction()
             .addToBackStack("listFragment")
@@ -19,9 +57,19 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    override fun onBackPressed() {
-        supportFragmentManager.popBackStack()
-        println(supportFragmentManager.backStackEntryCount)
+    override fun onDestroy() {
+        super.onDestroy()
+        supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        updateOnUI()
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
 
